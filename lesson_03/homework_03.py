ed105002db18711a5a8acd08d6b694dfb6086c10
iurii <immirg@gmail.com>
2024-09-26 15:52:05 +0300

    Added line breaks to alice_in_wonderland for better readability

--- lesson_03/homework_03.py
+++ lesson_03/homework_03.py
@@ -5,7 +5,10 @@ import math
 # task 02 == Знайдіть та відобразіть всі символи одинарної лапки (') у тексті
 # task 03 == Виведіть змінну alice_in_wonderland на друк
 
-alice_in_wonderland = '"Would you tell me, please, which way I ought to go from here?"\n"That depends a good deal on where you want to get to," said the Cat.\n"I don\'t much care where ——" said Alice.\n"Then it doesn\'t matter which way you go," said the Cat.\n"—— so long as I get somewhere," Alice added as an explanation.\n"Oh, you\'re sure to do that," said the Cat, "if you only walk long enough."'
+alice_in_wonderland = '"Would you tell me, please, which way I ought to go from here?"\n"That depends a good deal on ' \
+                      'where you want to get to," said the Cat.\n"I don\'t much care where ——" said Alice.\n"Then it ' \
+                      'doesn\'t matter which way you go," said the Cat.\n"—— so long as I get somewhere," Alice added ' \
+                      'as an explanation.\n"Oh, you\'re sure to do that," said the Cat, "if you only walk long enough."'
 print(alice_in_wonderland)
 # task 01 Не совсем понял условие задачи, так как в задании уже проставлены переносы на новую строку в тексте - \n
 # task 02 Я так понял что нужно экранировать одинарные кавычки в тексте. Сделал
